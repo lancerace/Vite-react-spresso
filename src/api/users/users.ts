ec57a2f521fda5c82e9e97093445381172c9b665
lancerace <lancerace144@gmail.com>
2024-02-25 21:00:18 +0800

    test an api

--- src/api/users/users.ts
+++ src/api/users/users.ts
@@ -1,6 +1,8 @@
 import axios from "axios"
 
 
-export const getUsers = async () => (axios.get('https://jsonplaceholder.typicode.com/users'));
+//export const getUsers = async () => (axios.get('https://jsonplaceholder.typicode.com/users'));
 
+//testing
+export const getUsers = async () => (axios.get('https://pokeapi.co/api/v2/pokemon/charmander'));
 
--- src/components/Posts/PostList.tsx
+++ src/components/Posts/PostList.tsx
@@ -1,7 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+import { getUsers } from '../../api/users/users';
+import Spinner from '../UI/Spinner';
+
 const PostList = () => {
+  const { data, isLoading, isSuccess }: any = useQuery({ queryKey: ['getUsers'], queryFn: getUsers, staleTime: 300000 });
+
+  if (isLoading) return <Spinner />;
+
   return (
-    <div className='h-screen flex justify-center'>
-      <h1>Post List</h1>
+    <div className='h-screen flex justify-center items-center'>
+      <>
+        {isSuccess &&
+          [data.data].map((data: any, index) => {
+            return <div key={`post-${index}`}>{data.species.name}</div>;
+          })}
+      </>
     </div>
   );
 };
